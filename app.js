3b89212f67436e0cd83d1d2760e48188cc001236
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-08 22:41:25 +0530

    dotenv package used to set env variables and connected to db

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const PORT = 3000;
+const dontenv = require("dotenv");
+
+dontenv.config({ path: ".env" });
+const PORT = process.env.PORT;
+const dbURI = process.env.DATABASE_URL;
+
+const connect = mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connect.then(
+  (db) => {
+    console.log("Connected Successfully to Mongodb Server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
 const app = express();
 
-//root folder for the app is public
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.listen(PORT, () => {

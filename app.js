13f68c323670ec1eea3f14d4976b18de66d099c6
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-08 22:10:33 +0530

    blog schema created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const PORT = 3000;
 const app = express();

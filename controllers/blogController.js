1789d20202dad9396d90d7e65ee07536bf807643
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-15 19:42:34 +0530

    getAllBlogs, createBlogs and get blogbyid implemented

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,12 +1,46 @@
-const Blog = require("../models/blogs");
+const Blogs = require("../models/blogs");
 
 const getAllBlogs = (req, res, next) => {
-  res.status(200).json({ status: "get all blogs" });
+  Blogs.find({})
+    .then((blogs) => {
+      res.status(200);
+      res.setHeader("Content-Type", "application/json");
+      res.json({ blogs: blogs, currentUser: "" });
+    })
+    .catch((err) => {
+      res.status(500);
+      res.json({ error: err });
+    });
 };
 
 const createBlog = (req, res, next) => {
-  res.status(200).json({ status: "create blog" });
+  Blogs.create(req.body)
+    .then((blog) => {
+      res.status(200);
+      res.setHeader("Content-Type", "application/json");
+      res.json({ status: "Blog added successfully", data: blog });
+    })
+    .catch((err) => {
+      res.status(404);
+      res.json({ message: "Invalid Object Property", error: err });
+    });
 };
 
-module.exports.getAllBlogs = getAllBlogs;
-module.exports.createBlog = createBlog;
+const getblogById = (req, res, next) => {
+  Blogs.findById(req.params.blogId)
+    .then((blog) => {
+      res.status(200);
+      res.setHeader("Content-Type", "application/json");
+      res.json(blog);
+    })
+    .catch((err) => {
+      res.status(404);
+      res.json({ message: "Id did not exists", error: err });
+    });
+};
+
+module.exports = {
+  getAllBlogs,
+  createBlog,
+  getblogById,
+};
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { getAllBlogs, createBlog } = require("../controllers/blogController");
+const {
+  getAllBlogs,
+  createBlog,
+  getblogById,
+} = require("../controllers/blogController");
 
 const blogRoute = express.Router();
 
 blogRoute.route("/").get(getAllBlogs).post(createBlog);
+blogRoute.route("/:blogId").get(getblogById);
 
 module.exports = blogRoute;

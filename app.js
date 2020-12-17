d5d04bc1a9f8c62b92a79ef62e1623280324da97
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-17 15:32:53 +0530

    installed multer and done basic config

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dontenv = require("dotenv");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const blogRouter = require("./routes/blogRoutes");
 
+// env config
 dontenv.config({ path: ".env" });
 const PORT = process.env.PORT;
 const dbURI = process.env.DATABASE_URL;
@@ -13,6 +17,25 @@ const connect = mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+//multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var directory = path.join(__dirname, "uploads");
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory);
+    }
+    cb(null, directory);
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + "." + file.originalname.split(".")[1]
+    );
+  },
+});
+
+const upload = multer({ storage: storage }).single("avatar");
 
 connect.then(
   (db) => {

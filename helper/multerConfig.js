1d7c9b636d2e42f24eea76c1674937f8523cb2b4
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-17 15:37:09 +0530

    multer config added in helper file

---
+++ helper/multerConfig.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var directory = path.join(__dirname, "uploads");
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory);
+    }
+    cb(null, directory);
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + "." + file.originalname.split(".")[1]
+    );
+  },
+});
+
+const upload = multer({ storage: storage }).single("avatar");
+
+module.exports = upload;
